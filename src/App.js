aa7ff3e976d60314c3e6eb2ec4c0a4c837cdba9e
Jared Kim <jaredkim01@yahoo.com>
2023-04-23 22:55:28 -0500

    starting files, start with particlesjs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Particles from './components/particles';
+
+function App() {
+  return (
+    <div className="App">
+      <Particles />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/particles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Particles() {
+  
+
+  return (
+    <div>
+      <h1>particles js</h1>
+      
+    </div>
+  )
+}
+
+export default Particles;
